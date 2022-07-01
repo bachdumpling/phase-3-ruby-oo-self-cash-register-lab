293c0b9a1ca33335f4c4d27d87f3cd8ffb7d396f
Bach Le <lehoangbach7802@gmail.com>
2022-07-01 11:38:11 -0400

    commit tree a29f30f8383b69ece89a6f4f548577b695843116

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount, :amount, :last_transaction, :items
+
+    def initialize(total = 0, discount = 20, items = [])
+        @total = total
+        @discount = discount
+        @items = items
+    end
+
+    def add_item(title, amount, quantity)
+        self.last_transaction = self.amount * self.quantity
+        self.total += self.last_transaction
+        quantity.times do
+        self.items << self.title
+        end
+    end
+
+end
+
+binding.pry
